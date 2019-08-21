d5c22e87d4c24905afb931f32d8da1180a39acc7
Faruk@12 <ozbekfaruk12@gmail.com>
2019-08-21 10:51:57 +0300

    trying the solve heroku problems

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios istekleri için bir base location belirle
 var instance = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://todosmv.herokuapp.com/"
 });
 
 
