457431057c253225fd46b0145386a9e697f57a9f
Faruk@12 <ozbekfaruk12@gmail.com>
2019-08-21 11:42:16 +0300

    fixing buttons

--- client/src/components/utils/Button.js
+++ client/src/components/utils/Button.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import './Button.scss'
 
-const Button = ({name,action}) => {
+const Button = ({name,action,filterName,filter}) => {
     return (
-        <button onClick={action} className="button">{name}</button>
+        <button 
+            onClick={action} 
+            className="button"
+            style={{ color: `${filterName === filter ? '#03a9f4' : '#eee'}`}}
+        >
+        {name}
+        </button>
     )
 }
-
-export default Button
+const mapStateToProps = state => ({
+    filter:state.filter,
+})
+export default connect(mapStateToProps)(Button)
