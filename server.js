19cd77d9df0debc63061b45102d64d21ffd83f7a
Faruk@12 <ozbekfaruk12@gmail.com>
2019-08-21 11:56:51 +0300

    fixing button

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const cors = require('cors');
 const path = require('path');
 const todo = require('./routes/api/todo');
 
-const keys = require('./keys');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const db = keys.mongoURI;
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
